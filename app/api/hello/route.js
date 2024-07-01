352e4dd73e679c11e610c917b9280a0714270e84
HamdiTliba <hamditliba.go@gmail.com>
2024-07-01 14:33:20 +0100

    remove api

--- app/api/hello/route.js
+++
@@ -1,9 +0,0 @@
-import { getRequestContext } from "@cloudflare/next-on-pages";
-
-export const runtime = "edge";
-
-export async function GET(request) {
-  let responseText = "Hello World";
-
-  return new Response(responseText);
-}
